2aee27ebfd3d304612465ab66541a9003575f19e
Anthony Gunardi <anthonygunardi@gmail.com>
2024-02-13 13:42:29 +0700

    Update users route

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,8 @@ const route = require('express').Router();
 const { UserController } = require('../controllers');
 const authentication = require('../middlewares/userAuthentication');
 
-route.get('/', authentication, UserController.findAllUser); // with query params ?role=participant / admin 
+route.get('/', authentication, UserController.findAllUser);
 route.post('/register', UserController.register);
-// route.post('/registeradmin', UserController.registerAdmin);
 route.post('/login', UserController.login);
-// route.get('/:id', UserController.findUserById);
 
 module.exports = route;
