1f8ed7cc6870188d75675414820211014b32b171
kiigeeks <marjukiiahmad@gmail.com>
2024-02-15 10:15:45 +0700

    update position, add model post & event

---
+++ migrations/20240214031058-create-events.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      slug: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate:{
+            notEmpty: true
+        }
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      point: {
+        type: Sequelize.INTEGER
+      },
+      publishied_at: {
+        type: Sequelize.DATE
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Posts',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  }
+};
---
+++ routes/positions.js
@@ -0,0 +1,9 @@
+const route = require('express').Router();
+const { PositionController } = require('../controllers');
+const authentication = require('../middlewares/userAuthentication');
+
+route.get('/', PositionController.getPositions);
+route.post('/', PositionController.addPosition);
+route.get("/:uuid", PositionController.getPosition);
+
+module.exports = route;
