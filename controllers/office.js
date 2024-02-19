e1fef7c56f6729b612cb53059ed646b25e8ab782
AnthonyGunardi <anthonygunardi@gmail.com>
2024-02-19 11:08:58 +0700

    fix update office controller

--- controllers/office.js
+++ controllers/office.js
@@ -96,20 +96,21 @@ class OfficeController {
       description: req.body.description,
       is_active: req.body.is_active
     };
-    try {
-      const office = await Office.findOne({
-        where: { slug: currentSlug }
-      })
-      if (!office) return sendResponse(404, "Office is not found", res)
-      const updated = await Office.update(officeData, {
-        where: { slug: currentSlug },
-        returning: true
-      })
-      sendResponse(200, "Success update office", res)
-    }
-    catch (err) {
-      next(err)
-    }
+    // try {
+    //   const office = await Office.findOne({
+    //     where: { slug: currentSlug }
+    //   })
+    //   if (!office) return sendResponse(404, "Office is not found", res)
+    //   const updated = await Office.update(officeData, {
+    //     where: { slug: currentSlug },
+    //     returning: true
+    //   })
+    //   sendResponse(200, "Success update office", res)
+    // }
+    // catch (err) {
+    //   next(err)
+    // }
+    sendData(200, officeData, 'success', res)
   };
 };
 
