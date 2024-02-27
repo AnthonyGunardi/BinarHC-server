450721a745ac7c5f51ad35fdff4dafdf621059d7
AnthonyGunardi <anthonygunardi@gmail.com>
2024-02-27 22:37:59 +0700

    remove unneeded comments in post controller & route

--- controllers/post.js
+++ controllers/post.js
@@ -11,112 +11,66 @@ class PostController {
   static async create(req, res, next) {
     const email = req.body.email
     const userEmail = req.user.email
-    const file = req.file?.path || null;
-    // try {
-    //   const user = await User.findOne({ 
-    //     where: { email } 
-    //   });
-    //   if (!user || user.email != userEmail) return sendResponse(404, "User is not found", res);
-    //   const postData = {
-    //     title: req.body.title, 
-    //     slug: req.body.slug,
-    //     thumbnail: file,
-    //     description: req.body.description,
-    //     type: req.body.type,
-    //     published_at: req.body.published_at || new Date(),
-    //     user_id: user.id,
-    //     is_active: req.body.is_active
-    //   };
-    //   const post = await Post.findOne({ 
-    //     where: { 
-    //       slug: postData.slug
-    //     } 
-    //   });
-    //   if (!Boolean(post)) {
-    //     const newPost = await Post.create(postData);
-    //     const { id, name, description, slug } = newPost;
-    //     if (postData.type == 'event') {
-    //       const newEvent = await Event.create({ post_id: id, is_active: false });
-    //     }
-    //     sendData(201, { id, name, description, slug }, "Success create post", res);  
-    //   } else {
-    //     sendResponse(400, 'Post already exist', res);
-    //   }
-    // }
-    // catch (err) {
-    //   await unlinkAsync(req.file.path)
-    //   next(err)
-    // };
-  };
-
-  static async create2(req, res, next) {
-    // if (req.files === null) return sendResponse(400, "Image null", res) 
-    const email = req.body.email
-    const userEmail = req.user.email
     let url = null
+    //upload file if req.files isn't null
     if (req.files !== null) {
       const file = req.files.file;
-    const fileSize = file.data.length;
-    const ext = path.extname(file?.name);
-    const fileName = file.md5 + ext;
-    url = `thumbnail-posts/${fileName}`;
+      const fileSize = file.data.length;
+      const ext = path.extname(file.name);
+      const fileName = file.md5 + ext;
+      const allowedType = ['.png', '.jpg', '.jpeg'];
+      url = `thumbnail-posts/${fileName}`;
 
-    //validation type file
-    const allowedType = ['.png', '.jpg', '.jpeg'];
-    if(!allowedType.includes(ext.toLocaleLowerCase())) return sendResponse(422, "File must be image with extension png, jpg, jpeg", res)
-    
-    //validation size file max 5mb
-    if(fileSize > 5000000) return sendResponse(422, "Image must be less than 5 mb", res)
-    
-    file.mv(`./public/images/thumbnail-posts/${fileName}`, async (err) => {
-        //validation process upload file to server
+      //validate file type
+      if(!allowedType.includes(ext.toLocaleLowerCase())) return sendResponse(422, "File must be image with extension png, jpg, jpeg", res)    
+      //validate file size max 5mb
+      if(fileSize > 5000000) return sendResponse(422, "Image must be less than 5 mb", res)
+      //place the file on server
+      file.mv(`./public/images/thumbnail-posts/${fileName}`, async (err) => {
         if(err) return sendResponse(502, err.message, res)
       })
     }
     
-        try {
-          const user = await User.findOne({ 
-            where: { email } 
-          });
-          if (!user || user.email != userEmail) return sendResponse(404, "User is not found", res);
-          const postData = {
-            title: req.body.title, 
-            slug: req.body.slug,
-            thumbnail: url,
-            description: req.body.description,
-            type: req.body.type,
-            published_at: req.body.published_at,
-            user_id: user.id,
-            is_active: req.body.is_active
-          };
-          const post = await Post.findOne({ 
-            where: { 
-              slug: postData.slug
-            } 
-          });
-
-          if (!Boolean(post)) {
-            const newPost = await Post.create(postData);
-            const { id, name, description, slug, thumbnail } = newPost;
-            sendData(201, { id, name, description, slug, thumbnail }, "Success create post", res);  
-          } else {
-            sendResponse(400, 'Post already exist', res);
-          }
-        }
-        catch (err) {
-          // await unlinkAsync(req.file.path)
-          next(err)
-        };
- 
+    try {
+      const user = await User.findOne({ 
+        where: { email } 
+      });
+      if (!user || user.email != userEmail) return sendResponse(404, "User is not found", res);
+      const postData = {
+        title: req.body.title, 
+        slug: req.body.slug,
+        thumbnail: url,
+        description: req.body.description,
+        type: req.body.type,
+        published_at: req.body.published_at,
+        user_id: user.id,
+        is_active: req.body.is_active
+      };
+      const post = await Post.findOne({ 
+        where: { 
+          slug: postData.slug
+        } 
+      });
+      if (!Boolean(post)) {
+        const newPost = await Post.create(postData);
+        const { id, name, description, slug, thumbnail } = newPost;
+        sendData(201, { id, name, description, slug, thumbnail }, "Success create post", res);  
+      } else {
+        sendResponse(400, 'Post already exist', res);
+      }
+    }
+    catch (err) {
+      next(err)
+    };
   };
 
   static async getAllPosts(req, res, next) {
     try {
-        const posts = await Post.findAll({
-          attributes:['title', 'slug', 'thumbnail', 'description', 'type', 'is_active', 'published_at', 'createdAt'],
-          order: [['createdAt', 'desc']]
-        });
-        sendData(200, posts, "Success get all posts", res);
+      const posts = await Post.findAll({
+        attributes:['title', 'slug', 'thumbnail', 'description', 'type', 'is_active', 'published_at', 'createdAt'],
+        order: [['createdAt', 'desc']]
+      });
+      sendData(200, posts, "Success get all posts", res);
     } 
     catch (err) {
         next(err)
@@ -126,19 +80,19 @@ class PostController {
   static async getPost(req, res, next) {
     const slug = req.params.slug
     try {
-        const post = await Post.findOne({
-            where: { slug },
-            attributes:['title', 'slug', 'thumbnail', 'description', 'type', 'is_active', 'published_at', 'createdAt'],
-            include: {
-              model: Event,
-              attributes:['title']
-            }
-        })
-        if (!post) return sendResponse(404, "Post is not found", res)
-        sendData(200, post, "Success get post data", res)
+      const post = await Post.findOne({
+        where: { slug },
+        attributes:['title', 'slug', 'thumbnail', 'description', 'type', 'is_active', 'published_at', 'createdAt'],
+        include: {
+          model: Event,
+          attributes:['title']
+        }
+      })
+      if (!post) return sendResponse(404, "Post is not found", res)
+      sendData(200, post, "Success get post data", res)
     } 
     catch (err) {
-        next(err)
+      next(err)
     }
   }
 
@@ -170,55 +124,6 @@ class PostController {
     const currentSlug = req.params.slug
     const email = req.body.email
     const userEmail = req.user.email
-    const file = req.file?.path || null;
-    try {
-      const user = await User.findOne({ 
-        where: { email } 
-      });
-      if (!user || user.email != userEmail) return sendResponse(404, "User is not found", res);
-      const postData = {
-        title: req.body.title, 
-        slug: req.body.slug,
-        thumbnail: file,
-        description: req.body.description,
-        type: req.body.type,
-        published_at: req.body.published_at || new Date(),
-        user_id: user.id,
-        is_active: req.body.is_active
-      };
-      const post = await Post.findOne({
-        where: { slug: currentSlug }
-      })
-      if (!post) return sendResponse(404, "Post is not found", res)
-      const postWithNewSlug = await Post.findOne({
-        where: { 
-          [Op.and]: [
-            { 
-              id: {
-                [Op.ne]: post.id, 
-              } 
-            },
-            { slug: postData.slug }
-        ]
-          }
-      })
-      if (postWithNewSlug) return sendResponse(403, "Slug already used", res)
-      const updated = await Post.update(postData, {
-        where: { id: post.id },
-        returning: true
-      })
-      sendResponse(200, "Success update post", res)
-    }
-    catch (err) {
-      next(err)
-    }
-  };
-
-  static async update2(req, res, next) {
-
-    const currentSlug = req.params.slug
-    const email = req.body.email
-    const userEmail = req.user.email
 
     try {
       const user = await User.findOne({ 
@@ -234,27 +139,26 @@ class PostController {
       if(req.files === null) {
         url = post.thumbnail;
       } else {
+        //upload file if req.files isn't null
         const file = req.files.thumbnail;
         const fileSize = file.data.length;
-        const ext = path.extname(file?.name);
+        const ext = path.extname(file.name);
         const fileName = file.md5 + ext;
+        const allowedType = ['.png', '.jpg', '.jpeg'];
         url = `thumbnail-posts/${fileName}`;
     
-        //validation type file
-        const allowedType = ['.png', '.jpg', '.jpeg'];
+        //validate file type
         if(!allowedType.includes(ext.toLocaleLowerCase())) return sendResponse(422, "File must be image with extension png, jpg, jpeg", res)
-        
-        //validation size file max 5mb
+        //validate file size max 5mb
         if(fileSize > 5000000) return sendResponse(422, "Image must be less than 5 mb", res)
-        
+        //place the file on server
         file.mv(`./public/images/thumbnail-posts/${fileName}`, async (err) => {
-            //validation process upload file to server
-            if(err) return sendResponse(502, err.message, res)
+          if(err) return sendResponse(502, err.message, res)
         })
-      if (post.thumbnail !== null) {
-        const filePath = `./public/images/${post.thumbnail}`;
-        fs.unlinkSync(filePath);
-      }
+        if (post.thumbnail !== null) {
+          const filePath = `./public/images/${post.thumbnail}`;
+          fs.unlinkSync(filePath);
+        }
       }
       const timeStamp = createTimeStamp(req.body.scheduleDate, req.body.scheduleTime)
       const date = new Date(timeStamp)
@@ -264,7 +168,6 @@ class PostController {
         thumbnail: url,
         description: req.body.description,
         type: req.body.type,
-        // published_at: req.body.published,
         published_at: date,
         user_id: user.id,
         is_active: req.body.is_active
--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,11 @@
 const route = require('express').Router();
 const { PostController } = require('../controllers');
 const authentication = require('../middlewares/userAuthentication');
-const multer = require('multer');
-const diskStorage = require('../config/diskStorage');
 
 route.get('/', authentication, PostController.getAllPosts);
-// route.post('/', authentication, multer({ storage: diskStorage }).single("thumbnail"), PostController.create);
-route.post('/', authentication, PostController.create2);
+route.post('/', authentication, PostController.create);
 route.get('/:slug', authentication, PostController.getPost);
-// route.put('/:slug', authentication, multer({ storage: diskStorage }).single("thumbnail"), PostController.update);
-route.put('/:slug', authentication, PostController.update2);
+route.put('/:slug', authentication, PostController.update);
 route.put('/toggle/:slug', authentication, PostController.togglePost);
 
 module.exports = route;
