38d9ff29348a2e0df93ddf3c2a0fe9f5e5988a58
AnthonyGunardi <anthonygunardi@gmail.com>
2024-03-10 13:28:10 +0700

    create post gallery route

---
+++ routes/posts_gallery.js
@@ -0,0 +1,10 @@
+const route = require('express').Router();
+const { PostGalleryController } = require('../controllers');
+const authentication = require('../middlewares/userAuthentication');
+
+route.get('/gallery/:id', authentication, PostGalleryController.getGallery);
+route.get('/:slug', authentication, PostGalleryController.getGalleries);
+route.post('/:slug', authentication, PostGalleryController.create);
+route.put('/:id', authentication, PostGalleryController.update);
+
+module.exports = route;
