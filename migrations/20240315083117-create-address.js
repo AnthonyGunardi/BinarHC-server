49f4116337db72ef50174d5bb27acc157f3c83a7
AnthonyGunardi <anthonygunardi@gmail.com>
2024-03-18 07:16:52 +0700

    fix address migration

--- migrations/20240315083117-create-address.js
+++ migrations/20240315083117-create-address.js
@@ -21,15 +21,6 @@ module.exports = {
       is_main: {
         type: Sequelize.BOOLEAN
       },
-      village_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'indonesia_villages',
-          key: 'id'
-        },
-        onUpdate: 'cascade',
-        onDelete: 'cascade'
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -38,8 +29,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    }, {
-      logging: console.log
     });
   },
   async down(queryInterface, Sequelize) {
