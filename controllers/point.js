635e4fed8a245cb83a86e16adc92065560d415df
AnthonyGunardi <anthonygunardi@gmail.com>
2024-05-03 09:30:04 +0700

    add modifyByJoin method in Point Controller & route

--- controllers/point.js
+++ controllers/point.js
@@ -44,6 +44,45 @@ class PointController {
       next(err)
     };
   }
+
+  static async modifyByJoin(req, res, next) {
+    try {
+      const nip = req.user.nip;
+      const { type, point, description } = req.body;
+
+      //get user_id
+      const user = await User.findOne({ where: { nip } });
+      if (!Boolean(user)) return sendResponse(404, "User is not found", res)
+
+      //get current point balance
+      const currentPoint = await Point.findOne({ where: { user_id: user.id } });
+      if (!Boolean(currentPoint)) return sendResponse(404, "Point data is not found", res)
+      
+      //get updated point balance
+      let updatedBalance;
+      if (type == 'revenue') {
+        updatedBalance = parseInt(currentPoint.balance) + parseInt(point)
+      } else if (type == 'expense') {
+        updatedBalance = parseInt(currentPoint.balance) - parseInt(point)
+      } else {
+        return sendResponse(400, "Point Log type is required", res)
+      }
+
+      //check if point balance is smaller than zero
+      if (parseInt(updatedBalance) < 0) return sendResponse(400, "Point can't get smaller than zero", res)
+
+      await Point.update(
+        { balance: updatedBalance }, 
+        { where: { user_id: currentPoint.user_id } })
+      await Point_Log.create(
+        { type, point, description, user_id: currentPoint.user_id, last_balance: parseInt(currentPoint.balance) }
+      );
+      sendResponse(200, "Success update point", res)
+    }
+    catch (err) {
+      next(err)
+    };
+  }
 }
 
 module.exports = PointController;
--- routes/points.js
+++ routes/points.js
@@ -3,6 +3,7 @@ const { PointController } = require('../controllers');
 const authentication = require('../middlewares/userAuthentication');
 const adminAuthorization = require('../middlewares/adminAuthorization');
 
+route.put('/join', authentication, PointController.modifyByJoin);
 route.put('/:nip', authentication, adminAuthorization, PointController.modify);
 
 module.exports = route;
