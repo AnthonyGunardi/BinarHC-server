fe7a7dd9f1f610ea16df077ff4d29f1f009477de
AnthonyGunardi <anthonygunardi@gmail.com>
2024-10-30 09:58:19 +0700

    add getUserAttendances method on user controller & route

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -157,59 +157,6 @@ class AttendanceController {
     }
   };
 
-  static async createByAdmin(req, res, next) {
-    try {
-      const userEmail = req.user.email
-      const { start_date, end_date, type, nip, note } = req.body;
-
-      //get admin_id
-      const admin = await User.findOne({ 
-        where: { email: userEmail } 
-      });
-      if (!admin) return sendResponse(404, "User is not found", res);
-
-      //check if user is exist
-      const user = await User.findOne({ 
-        where: { nip } 
-      });
-      if (!user) return sendResponse(404, "Employee is not found", res);
-
-      //check if absence already exist
-      const absence = await Absence.findOne({ 
-        where: { start_date, end_date, employee_id: user.id } 
-      });
-      if (Boolean(absence)) return sendResponse(400, 'Absence request already exist', res);
-
-      //upload file if req.files isn't null
-      let url = null
-      if (req.files != null) {
-        const file = req.files.photo;
-        const fileSize = file.data.length;
-        const ext = path.extname(file.name);
-        const fileName = file.md5 + ext;
-        const allowedType = ['.png', '.jpg', '.jpeg'];
-        url = `absence-requests/${fileName}`;
-
-        //validate file type
-        if(!allowedType.includes(ext.toLocaleLowerCase())) return sendResponse(422, "File must be image with extension png, jpg, jpeg", res)    
-        //validate file size max 5mb
-        if(fileSize > 5000000) return sendResponse(422, "Image must be less than 5 mb", res)
-        //place the file on server
-        file.mv(`./public/images/absence-requests/${fileName}`, async (err) => {
-          if(err) return sendResponse(502, err.message, res)
-        })
-      }
-
-      const newAbsence = await Absence.create(
-        { start_date, end_date, photo: url, type, status: 'success',note, employee_id: user.id, admin_id: admin.id }
-      );
-      sendData(201, { id: newAbsence.id, start_date: newAbsence.start_date, end_date: newAbsence.end_date, type: newAbsence.type, status: newAbsence.status, note: newAbsence.note, employee_id: newAbsence.employee_id }, "Pengajuan absen berhasil", res);  
-    }
-    catch (err) {
-      next(err)
-    };
-  };
-
   static async getAllAbsences(req, res, next) {
     try {
       const { division_slug, start_date, end_date } = req.query;
@@ -285,119 +232,26 @@ class AttendanceController {
     };
   };
 
-  static async getAbsencesByScroll(req, res, next) {
+  static async getUserAttendancesByScroll(req, res, next) {
     try {
       const lastID = parseInt(req.query.lastID) || 0;
       const limit = parseInt(req.query.limit) || 0;
-      const type = req.query.type || "";
+      const nip = req.query.nip;
       let result = [];
 
-      if (lastID < 1) {
-        //get absences where its type is like keyword
-        const results = await Absence.findAll({
-          where: {
-            type: {
-              [Op.like]: '%'+type+'%'
-            },
-          },
-          attributes: {
-            exclude: ['employee_id', 'admin_id']
-          },
-          include: [
-            {
-              model: User,
-              as: 'Absence_Requester',
-              attributes: {
-                exclude: ['createdAt', 'updatedAt']
-              }
-            },
-            {
-              model: User,
-              as: 'Absence_Approver',
-              attributes: {
-                exclude: ['createdAt', 'updatedAt']
-              }
-            },
-          ],
-          limit: limit,
-          order: [
-            ['id', 'DESC']
-          ]
-        })
-        result = results
-      } else {
-        //get absences where its type is like keyword
-        const results = await Absence.findAll({
-          where: {
-            id: {
-              [Op.lt]: lastID
-            },
-            status: {
-              [Op.like]: '%'+type+'%'
-            },
-          },
-          attributes: {
-            exclude: ['employee_id']
-          },
-          include: {
-            model: User,
-            as: 'Overtime_Requester',
-            attributes: {
-              exclude: ['createdAt', 'updatedAt']
-            }
-          },
-          limit: limit,
-          order: [
-            ['id', 'DESC']
-          ]
-        })
-        result = results
-      }
-      
-      const payload = {
-        datas: result,
-        lastID: result.length ? result[result.length - 1].id : 0,
-        hasMore: result.length >= limit ? true : false
-      };
-      sendData(200, payload, "Success get absences data", res)
-    } 
-    catch (err) {
-        next(err)
-    };
-  };
-
-  static async getUserAbsencesByScroll(req, res, next) {
-    try {
-      const lastID = parseInt(req.query.lastID) || 0;
-      const limit = parseInt(req.query.limit) || 0;
-      const type = req.query.type || "";
-      const userEmail = req.user.email;
-      let result = [];
-
-      //get user_id
+      // get user_id
       const user = await User.findOne({ 
-        where: { email: userEmail } 
+        where: { nip }
       });
       if (!user) return sendResponse(404, "User is not found", res);
 
       if (lastID < 1) {
-        //get absences where its type is like keyword
-        const results = await Absence.findAll({
+        const results = await Attendance.findAll({
           where: {
-            type: {
-              [Op.like]: '%'+type+'%'
-            },
-            employee_id: user.id
+            user_id: user.id,
           },
           attributes: {
-            exclude: ['employee_id']
-          },
-          include: {
-            model: User,
-            as: 'Absence_Requester',
-            attributes: {
-              exclude: ['createdAt', 'updatedAt']
-            }
+            exclude: ['user_id']
           },
           limit: limit,
           order: [
@@ -406,25 +260,15 @@ class AttendanceController {
         })
         result = results
       } else {
-        //get absences where its type is like keyword
-        const results = await Absence.findAll({
+        const results = await Attendance.findAll({
           where: {
             id: {
               [Op.lt]: lastID
             },
-            status: {
-              [Op.like]: '%'+type+'%'
-            },
+            user_id: user.id
           },
           attributes: {
-            exclude: ['employee_id']
-          },
-          include: {
-            model: User,
-            as: 'Overtime_Requester',
-            attributes: {
-              exclude: ['createdAt', 'updatedAt']
-            }
+            exclude: ['user_id']
           },
           limit: limit,
           order: [
@@ -439,7 +283,7 @@ class AttendanceController {
         lastID: result.length ? result[result.length - 1].id : 0,
         hasMore: result.length >= limit ? true : false
       };
-      sendData(200, payload, "Success get absences data", res)
+      sendData(200, payload, "Success get user attendances data", res)
     } 
     catch (err) {
         next(err)
