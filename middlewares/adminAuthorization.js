e6e4ece28e2a8fe8a58f59e0c89c0f51e1505394
AnthonyGunardi <anthonygunardi@gmail.com>
2024-12-09 10:18:01 +0700

    fix authorization middleware

--- middlewares/adminAuthorization.js
+++ middlewares/adminAuthorization.js
@@ -1,6 +1,6 @@
 function adminAuthorization (req, res, next) {
   const role = req.user.is_admin
-  if (role === 'admin' || role === 'superadmin') {
+  if (role === 'attendance' || role === 'moderator' || role === 'admin' || role === 'superadmin') {
     next()
   } else {
     res.status(403).json({ message: "You are unauthorized to do this action!" })
