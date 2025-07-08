0b262d89fc5ee2ccb26d4aba06218107a7a82e10
AnthonyGunardi <anthonygunardi@gmail.com>
2025-07-08 13:51:21 +0700

    fix GMT+7 problem in getRewardsByScroll method

--- controllers/reward.js
+++ controllers/reward.js
@@ -74,7 +74,9 @@ class RewardController {
       const lastID = parseInt(req.query.lastID) || 0;
       const limit = parseInt(req.query.limit) || 0;
       const search = req.query.key || "";
-      const today = new Date();
+      // Menyesuaikan waktu ke GMT+7
+      const offset = 7 * 60; // GMT+7 in minute
+      const today = new Date(now.getTime() + (offset - now.getTimezoneOffset()) * 60000);
       let result = [];
 
       if (lastID < 1) {
